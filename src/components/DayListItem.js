2d24eaebfd78d0856416af5e86d48a4267513849
Albert Ho <albertcyi.ho@gmail.com>
2022-02-22 14:41:46 -0800

    Feat - add container component DayList

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,23 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots,
   });
+
+  const formatSpots = spots => {
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+
+    if (spots === 0) {
+      return "no spots remaining";
+    }
+
+    return `${spots} spots remaining`;
+  };
+
   return (
     <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
