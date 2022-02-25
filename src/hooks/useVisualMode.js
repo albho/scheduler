ed72161714510d337dfa8c0a2d10cb246951593f
Albert Ho <albertcyi.ho@gmail.com>
2022-02-25 13:12:23 -0800

    Feat - create custom hook useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function (newMode, replace = false) {
+    if (replace) {
+      return setMode(newMode);
+    }
+
+    setMode(newMode);
+
+    setHistory(prev => [...prev, newMode]);
+  };
+
+  const back = function () {
+    if (history.length < 2) {
+      return;
+    }
+
+    const prevMode = history[history.length - 2];
+    setMode(prevMode);
+
+    setHistory(prev => prev.slice(0, -1));
+  };
+
+  return { mode, transition, back };
+}
