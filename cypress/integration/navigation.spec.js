42bde2c319d076de0eb9a968ec8b7c7ee441f32a
Albert Ho <albertcyi.ho@gmail.com>
2022-03-02 15:43:35 -0800

    Test - add tests for navigating days and booking/editing/canceling interviews

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,19 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+
     cy.visit("/");
+
+    cy.contains("Monday");
+  });
+
+  it("should visit root", () => {
+    cy.contains("[data-testid=day]", "Tuesday");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
